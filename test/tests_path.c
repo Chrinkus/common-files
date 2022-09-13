22789b79d2a3aa303d9f2bd251bc90267c7fde0c
Chris Schick <seeschickrun@gmail.com>
2022-09-12 21:38:51 -0500

    Added append test

--- test/tests_path.c
+++ test/tests_path.c
@@ -13,10 +13,32 @@ path_init_test(void** state)
         assert_int_equal(p1.len, 0);
 }
 
+static void
+path_append_test(void** state)
+{
+        (void)state;
+
+        struct cf_path p1;
+        cf_path_init(&p1);
+
+        assert_non_null(cf_path_append(&p1, "home"));
+        assert_int_equal(p1.len, 5);
+        assert_string_equal(p1.path, "/home");
+
+        assert_non_null(cf_path_append(&p1, "user"));
+        assert_int_equal(p1.len, 10);
+        assert_string_equal(p1.path, "/home/user");
+
+        assert_non_null(cf_path_append(&p1, "Documents"));
+        assert_int_equal(p1.len, 20);
+        assert_string_equal(p1.path, "/home/user/Documents");
+}
+
 int main(void)
 {
         const struct CMUnitTest tests[] = {
                 cmocka_unit_test(path_init_test),
+                cmocka_unit_test(path_append_test),
         };
 
         return cmocka_run_group_tests(tests, NULL, NULL);
