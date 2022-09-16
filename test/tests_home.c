60156bf7c0c157e175f47585d84f4a473844d87a
Chris Schick <seeschickrun@gmail.com>
2022-09-15 21:43:15 -0500

    Added setup and teardown for home test
    Followed suggestion from @davidwesst to store the current home directory
    before the test.

--- test/tests_home.c
+++ test/tests_home.c
@@ -2,20 +2,52 @@
 
 #include "cf.h"
 
+#include <stdlib.h>
+#include <string.h>
+
+static int
+home_setup(void** state)
+{
+        char* home = getenv("HOME");
+        if (!home)
+                return -1;
+
+        char* p = malloc(strlen(home) + 1);
+        if (!p)
+                return -1;
+
+        strcpy(p, home);
+        *state = p;
+
+        return 0;
+}
+
+static int
+home_teardown(void** state)
+{
+        free(*state);
+        return 0;
+}
+
 static void
 get_home_test(void** state)
 {
-        (void)state;
+        const char* home = *(char**)state;
 
         struct cf_path p1 = { .path = "", .len = 0 };
         assert_non_null(cf_get_home(&p1));
         assert_false(p1.len == 0);
+        assert_string_not_equal(p1.path, "");
+
+        assert_string_equal(p1.path, home);
+        assert_int_equal(p1.len, strlen(home));
 }
 
 int main(void)
 {
         const struct CMUnitTest tests[] = {
-                cmocka_unit_test(get_home_test),
+                cmocka_unit_test_setup_teardown(get_home_test,
+                                home_setup, home_teardown),
         };
 
         return cmocka_run_group_tests(tests, NULL, NULL);
